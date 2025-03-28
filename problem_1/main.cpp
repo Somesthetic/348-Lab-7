9f5913fa0b6de67a31c02634276c41c35e91dc76
Somesthetic <forest-denton@ku.edu>
2025-03-27 21:42:26 -0500

    finished problem 1

--- problem_1/main.cpp
+++ problem_1/main.cpp
@@ -1,3 +1,29 @@
-//
-// Created by forestd on 3/27/25.
-//
+#include <iostream>
+
+int main(){
+  int score;
+  while (true){
+    std::cout << "Enter 0 or 1 to STOP\nEnter the NFL score: ";
+    std::cin >> score;
+    if (score <= 1){
+      return 0;
+    } // the test is just gonna be completely brute forcing it
+    for (int safety = (score / 2); safety >= 0; safety--){
+      for (int field = (score / 3); field >= 0; field--){
+        for (int td = (score / 6); td >= 0; td--){
+          for (int td_p = (score / 7); td_p>= 0; td_p--){
+            for (int td_p2 = (score / 8); td_p2>= 0; td_p2--){
+              if ((safety*2)+(field*3)+(td*6)+(td_p*7)+(td_p2*8) == score){
+                printf("%i TD + 2pt, %i TD + FG, %i TD, %i 3pt FG, %i Safety\n", td_p2, td_p, td, field, safety);
+              }
+            }
+          }
+        }
+      }
+    }
+
+
+
+
+  }
+}
