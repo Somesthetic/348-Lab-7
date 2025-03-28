7cb7b09c9bcb9e2d074b5587f8c2d03879c3f3eb
Somesthetic <forest-denton@ku.edu>
2025-03-27 19:53:58 -0500

    Problem 2 now complete

--- problem_2/main.cpp
+++ problem_2/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
 
 
 int main(){
@@ -7,54 +8,57 @@ int main(){
   char cur_unit;
   char con_unit;
   int converted_temp;
+  std::string temp_cat;
+  std::string warning;
   std::cout << "Input temperature: ";
   std::cin >> temperature;
-  std::cout << "Current unit (c, f, or k)";
+  std::cout << "Current unit (c, f, or k): ";
   std::cin >> cur_unit;
-  std::cout << "Unit to be converted to (c, f, or k)";
+  std::cout << "Unit to be converted to (c, f, or k): ";
   std::cin >> con_unit;
-  switch(cur_unit){  // all the stuff is done based on the current unit
+  switch(cur_unit) {
+    // all the stuff is done based on the current unit
     case 'c':
       if (temperature < 0){
-        char temp_cat[] = "Freezing";
-        char warning[] = "Stay indoors, turn on the heater";
+        temp_cat = "Freezing";
+        warning = "Stay indoors, turn on the heater";
       }else if ( temperature < 10){
-          char temp_cat[] = "Cold";
-          char warning[] = "Wear a jacket";
+        temp_cat = "Cold";
+        warning = "Wear a jacket";
       }else if ( temperature < 25){
-        char temp_cat[] = "Comfortable";
-        char warning[] = "Enjoy the beautiful weather!";
+        temp_cat = "Comfortable";
+        warning = "Enjoy the beautiful weather!";
       }else if ( temperature < 35){
-        char temp_cat[] = "Hot";
-        char warning[] = "Make sure to bring sunscreen!";
+        temp_cat = "Hot";
+        warning = "Make sure to bring sunscreen!";
       }else{
-        char temp_cat[] = "Extreme heat";
-        char warning[] = "Wear a jacket"; // at this point, the temp category and warning are fully decided, now time
+        temp_cat = "Extreme heat";
+        warning = "Wear a jacket"; // at this point, the temp category and warning are fully decided, now time
         // for the conversion
       }
-      switch (con_unit){  // something to finally convert the appropriate units
-        case 'k':
-          converted_temp = temperature + 273;
-        case 'f':
-          converted_temp = (temperature * (9/5)) + 32;
-      } // ends the temperature converter
+    switch (con_unit){  // something to finally convert the appropriate units
+      case 'k':
+        converted_temp = temperature + 273;
+      case 'f':
+        converted_temp = (temperature * (9/5)) + 32;
+    } // ends the temperature converter
 
     case 'f':
       if (temperature < 32){
-        char temp_cat[] = "Freezing";
-        char warning[] = "Stay indoors, turn on the heater";
+        temp_cat = "Freezing";
+        warning = "Stay indoors, turn on the heater";
       }else if ( temperature < 50){
-        char temp_cat[] = "Cold";
-        char warning[] = "Wear a jacket";
+        temp_cat = "Cold";
+        warning = "Wear a jacket";
       }else if ( temperature < 77){
-        char temp_cat[] = "Comfortable";
-        char warning[] = "Enjoy the beautiful weather!";
+        temp_cat = "Comfortable";
+        warning = "Enjoy the beautiful weather!";
       }else if ( temperature < 95){
-        char temp_cat[] = "Hot";
-        char warning[] = "Make sure to bring sunscreen!";
+        temp_cat = "Hot";
+        warning = "Make sure to bring sunscreen!";
       }else{
-        char temp_cat[] = "Extreme heat";
-        char warning[] = "Wear a jacket"; // at this point, the temp category and warning are fully decided, now time
+        temp_cat = "Extreme heat";
+        warning = "Wear a jacket"; // at this point, the temp category and warning are fully decided, now time
         // for the conversion
       }
     switch (con_unit){  // something to finally convert the appropriate units
@@ -67,20 +71,20 @@ int main(){
     switch(cur_unit){  // all the stuff is done based on the current unit
       case 'k':
         if (temperature < 273){
-          char temp_cat[] = "Freezing";
-          char warning[] = "Stay indoors, turn on the heater";
+          temp_cat = "Freezing";
+          warning = "Stay indoors, turn on the heater";
         }else if ( temperature < 283){
-          char temp_cat[] = "Cold";
-          char warning[] = "Wear a jacket";
+          temp_cat = "Cold";
+          warning = "Wear a jacket";
         }else if ( temperature < 298){
-          char temp_cat[] = "Comfortable";
-          char warning[] = "Enjoy the beautiful weather!";
+          temp_cat = "Comfortable";
+          warning = "Enjoy the beautiful weather!";
         }else if ( temperature < 308){
-          char temp_cat[] = "Hot";
-          char warning[] = "Make sure to bring sunscreen!";
+          temp_cat = "Hot";
+          warning = "Make sure to bring sunscreen!";
         }else{
-          char temp_cat[] = "Extreme heat";
-          char warning[] = "Wear a jacket"; // at this point, the temp category and warning are fully decided, now time
+          temp_cat = "Extreme heat";
+          warning = "Wear a jacket"; // at this point, the temp category and warning are fully decided, now time
           // for the conversion
         }
       switch (con_unit){  // something to finally convert the appropriate units
@@ -89,10 +93,11 @@ int main(){
         case 'f':
           converted_temp = (temperature * (9/5)) -236;
       } // ends the temperature converter
+    }  // the rest of the code is no longer dependant on user choices, just needs to print out the result
+    printf("Converted temp: %d\nTemperature category: %s\n Advisory: %s",
+           converted_temp, temp_cat.c_str(), warning.c_str());
+  }
 
-
-
-    }
 
 
 
